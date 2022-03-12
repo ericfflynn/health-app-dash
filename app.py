8c33b1c153a668e550856cf504c9a6673233a5e6
Eric J Flynn <89708758+ericfflynn@users.noreply.github.com>
2022-03-12 12:05:05 -0500

    add app.server

--- app.py
+++ app.py
@@ -37,6 +37,8 @@ external_stylesheets = [dbc.themes.BOOTSTRAP]
 
 app = Dash(__name__, external_stylesheets = external_stylesheets)
 
+server = app.server
+
 app.layout = html.Div(
     children=[
         html.Div(
